fa8b9c36de631c4c43c9ba3c2edc235afb9114b8
BeGeos <montepeloso.m@gmail.com>
2020-12-21 18:28:33 +0100

    Moved configuration

--- __init__.py
+++ __init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
 
-
+# App configuration
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///astropy.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Output Schema configuration
+# app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_AS_ASCII'] = False
