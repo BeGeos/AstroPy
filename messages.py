d9cb6ca8a189aed8d4e8ac1a8fc127d71c598b2b
BeGeos <montepeloso.m@gmail.com>
2020-12-21 18:26:54 +0100

    Added messages

---
+++ messages.py
@@ -0,0 +1,9 @@
+# Messages for API
+
+messages = {
+    'welcome': 'This is AstroPy',
+    'no argument': 'No argument passed in',
+    'not found': 'No record found',
+    'hello': 'Hello, There',
+    'bad request': '400 Bad request'
+}
