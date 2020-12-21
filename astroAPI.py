11910bd82037848019d751f2e8ef53081e35175b
BeGeos <montepeloso.m@gmail.com>
2020-12-21 18:27:43 +0100

    Minor changes

--- astroAPI.py
+++ astroAPI.py
@@ -1,45 +1,43 @@
 from __init__ import app
 from flask import request, jsonify
 from models import Constellation, Stars, ConstellationSchema, SingleStarSchema, StarSchema
-
-
-# App configuration
-app.config['DEBUG'] = True
+from messages import messages
+from werkzeug import exceptions
 
 # Schema init
 constellation_schema = ConstellationSchema()
 constellations_schema = ConstellationSchema(many=True)
 
 single_star_schema = SingleStarSchema()
-multiple_stars_schema = StarSchema(many=True)
+multiple_stars_schema = SingleStarSchema(many=True)
+
 
 # API logic
-
-default = [{'AstroPy': 'no argument'}]
-
-
 @app.route('/')
 def home():
-    return """<h1 style="text-align: center"> 
-                    This is AstroPy
-              </h1>"""
+    return jsonify({'Welcome': messages['welcome']})
 
 
 @app.route('/hello', methods=['GET'])
 def greetings():
     if not request.args:
-        return jsonify(default)
+        return jsonify({'message': messages['hello']})
 
-    response = {'first_name': request.args['name'], 'last_name': int(request.args['age'])}
+    response = {'Hello, There': [request.args['name'],
+                                 request.args['last']]}
     return jsonify(response)
 
 
 @app.route('/astropy/api/v1/constellation', methods=['GET'])
-def constellation():
+def get_constellation():
     if not request.args:
-        return jsonify(default)
+        return jsonify({'message': messages['no argument']})
 
-    c = request.args['c']
+    try:
+        c = request.args['c']
+    except exceptions.BadRequestKeyError:
+        return jsonify({'message': messages['bad request']})
+
     query_result = Constellation.query.filter_by(name=c).first_or_404()
     output = constellation_schema.dump(query_result)
 
@@ -51,7 +49,7 @@ def get_constellations_via_query():
     """ Parameters accepted: [quadrant as q, min_latitude as min, max_latitude as max] """
 
     if not request.args:
-        return jsonify(default)
+        return jsonify({'message': messages['no argument']})
 
     if 'q' in request.args and 'min' in request.args and 'max' in request.args:
         obj_query = Constellation.query.filter_by(quadrant=request.args['q'],
@@ -89,30 +87,30 @@ def get_constellations_via_query():
                                                       max_latitude=request.args['max'])
 
     if obj_query is None or len(obj_query.all()) == 0:
-        output = {'No record found': 404}
+        output = {'message': messages['not found']}
     else:
         output = constellations_schema.dump(obj_query)
 
-    return jsonify(output)
+    return jsonify({'constellation': output})
 
 
 @app.route('/astropy/api/v1/constellation/all')
 def get_all_constellations():
     _all = Constellation.query.all()
     output = constellations_schema.dump(_all)
-    return jsonify(output)
+    return jsonify({'constellations': output})
 
 
 @app.route('/astropy/api/v1/star')
 def get_star():
     if not request.args:
-        return jsonify(default)
+        return jsonify({'message': messages['no argument']})
 
     s = request.args['s']
     query_result = Stars.query.filter_by(star=s).first_or_404()
     output = single_star_schema.dump(query_result)
 
-    return jsonify(output)
+    return jsonify({'star': output})
 
 
 @app.route('/astropy/api/v1/star/all')
@@ -120,8 +118,10 @@ def get_all_stars():
     _all = Stars.query.all()
     output = multiple_stars_schema.dump(_all)
 
-    return jsonify(output)
+    return jsonify({'stars': output})
 
+
+# TODO get stars via query -- distance only
 
 if __name__ == '__main__':
     app.run()
