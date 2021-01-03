68d255ee41f3b52c56e074d3e4b133b5a4707a71
BeGeos <montepeloso.m@gmail.com>
2021-01-03 11:56:18 +0100

    Added wrapper functions

---
+++ wrappers.py
@@ -0,0 +1,32 @@
+from flask import request, jsonify
+from models import db, User, AuthKeys
+from functools import wraps
+from datetime import datetime
+
+
+def auth_key_required(func):
+    """API Key check for expired keys, invalid keys or calls' limit reached"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'appid' not in request.args:
+            return jsonify({'message': 'Missing token'})
+
+        api_key = request.args['appid']
+        check_auth = AuthKeys.query.filter_by(key=api_key).first()
+
+        if check_auth:
+            if check_auth.expiration_date <= datetime.utcnow().timestamp():
+                db.session.delete(check_auth)
+                db.session.commit()
+                return jsonify({'message': 'Expired token'})
+            current_calls = check_auth.user.calls
+            current_user = User.query.get(check_auth.user_id)
+            if current_calls == 0:
+                return jsonify({'message': 'You have reached the limit of requests'})
+            current_user.calls = current_calls - 1
+            db.session.commit()
+            return func(*args, **kwargs)
+
+        return jsonify({'message': 'Invalid token'}), 401
+
+    return wrapper
