6e22b8cd67d84b549d5b83cad6560c2f6efab3bb
BeGeos <montepeloso.m@gmail.com>
2021-01-05 19:01:12 +0100

    Added models for security check

--- models.py
+++ models.py
@@ -51,7 +51,11 @@ class User(db.Model):
     email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(128))
     calls = db.Column(db.Integer, default=1000)
+    attempts = db.Column(db.Integer, default=5)
+    confirmed = db.Column(db.Boolean, default=False)
     auth_key = db.relationship('AuthKeys', backref='user', lazy=True, uselist=False)
+    security_code = db.relationship('SecurityCodes', backref='user', lazy=True, uselist=False)
+    recovery = db.relationship('Recovery', backref='user', lazy=True, uselist=False)
     date_created = db.Column(db.DateTime)
 
     def __repr__(self):
@@ -66,6 +70,25 @@ class AuthKeys(db.Model):
 
     def __repr__(self):
         return self.key
+
+
+# Security models
+class SecurityCodes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    code = db.Column(db.Integer, nullable=False)
+    expiration = db.Column(db.Integer)
+
+
+# -- Recover password
+class Recovery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    url_extension = db.Column(db.String(24))
+    exp_date = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'http://localhost:5000/{self.url_extension}'
 
 
 # Schema Setup
