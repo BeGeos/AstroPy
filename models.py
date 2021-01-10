5a6284eaffe823a66d36105c23443e49bf019622
BeGeos <montepeloso.m@gmail.com>
2021-01-10 17:05:04 +0100

    Removed User models

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from marshmallow import fields
 from __init__ import app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from datetime import datetime
 
 # Database init
 db = SQLAlchemy(app)
@@ -45,50 +44,14 @@ class Stars(db.Model):
         return self.name
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), unique=True, nullable=False)
-    email = db.Column(db.String(128), nullable=False, unique=True)
-    password = db.Column(db.String(128))
-    calls = db.Column(db.Integer, default=1000)
-    attempts = db.Column(db.Integer, default=5)
-    confirmed = db.Column(db.Boolean, default=False)
-    auth_key = db.relationship('AuthKeys', backref='user', lazy=True, uselist=False)
-    security_code = db.relationship('SecurityCodes', backref='user', lazy=True, uselist=False)
-    recovery = db.relationship('Recovery', backref='user', lazy=True, uselist=False)
-    date_created = db.Column(db.DateTime)
-
-    def __repr__(self):
-        return self.username
-
-
 class AuthKeys(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
     key = db.Column(db.String(24))
     expiration_date = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return self.key
-
-
-# Security models
-class SecurityCodes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    code = db.Column(db.Integer, nullable=False)
-    expiration = db.Column(db.Integer)
-
-
-# -- Recover password
-class Recovery(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    url_extension = db.Column(db.String(24))
-    exp_date = db.Column(db.Integer)
-
-    def __repr__(self):
-        return f'http://localhost:5000/{self.url_extension}'
 
 
 # Schema Setup
