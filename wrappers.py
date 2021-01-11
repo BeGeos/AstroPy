366e8386606887a1524bf3c3d1b5f7a1dd51524f
BeGeos <montepeloso.m@gmail.com>
2021-01-11 14:32:34 +0100

    Improved key permissions

--- wrappers.py
+++ wrappers.py
@@ -1,7 +1,8 @@
 from flask import request, jsonify
-from models import db, User, AuthKeys
+from models import AuthKeys
 from functools import wraps
 from datetime import datetime, timezone
+from functions import update_call_count
 
 
 def auth_key_required(func):
@@ -16,15 +17,13 @@ def auth_key_required(func):
 
         if check_auth:
             if check_auth.expiration_date <= datetime.now(timezone.utc).timestamp():
-                # db.session.delete(check_auth)
-                # db.session.commit()
                 return jsonify({'message': 'Expired token'})
-            current_calls = check_auth.user.calls
-            current_user = User.query.get(check_auth.user_id)
+            if not check_auth.active:
+                return jsonify({'message': 'This api key is not active'})
+            current_calls = update_call_count(check_auth.user_id)
             if current_calls == 0:
                 return jsonify({'message': 'You have reached the limit of requests'})
-            current_user.calls = current_calls - 1
-            db.session.commit()
+
             return func(*args, **kwargs)
 
         return jsonify({'message': 'Invalid token'}), 401
