64d92a23bb0bc51dc9116d0bece3a6311f0c5507
BeGeos <montepeloso.m@gmail.com>
2021-02-08 16:38:35 +0100

    Minor changes to string outputs

--- functions.py
+++ functions.py
@@ -17,7 +17,7 @@ def sun_time_from_api(lat, lon):
     """ Send API call to OpenWeatherAPI for sunrise and sunset time at location """
 
     url = 'https://api.openweathermap.org/data/2.5/weather?'
-    api_key = ''
+    api_key = secret_keys['WEATHER_APIKEY']
     payload = {'lat': lat, 'lon': lon, 'appid': api_key}
     get_request = requests.get(url, params=payload)
     if get_request.status_code != 200:
@@ -37,32 +37,36 @@ def sun_time_from_api(lat, lon):
             'utc': timezone}
 
 
-def star_rising_time(ra, sun_times: dict):
+def star_rising_time(ra, sun_times: dict, right_ascension):
     vernal_equinox = datetime(2020, 3, 21)
 
     # From API call
     sunrise = int(sun_times['sunrise'])
     sunset = int(sun_times['sunset'])
 
+    today = datetime.today()
+    current_year = today.year
+
     # Theory of right ascension
     time_of_year = {
         vernal_equinox: ra,
-        datetime(2020, 4, 21): ra - 2,
-        datetime(2020, 5, 21): ra - 4,
-        datetime(2020, 6, 21): ra - 6,
-        datetime(2020, 7, 21): ra - 8,
-        datetime(2020, 8, 21): ra - 10,
-        datetime(2020, 9, 21): ra - 12,
-        datetime(2020, 10, 21): ra - 14,
-        datetime(2020, 11, 21): ra - 16,
-        datetime(2020, 12, 21): ra - 18,
-        datetime(2020, 1, 21): ra - 20,
-        datetime(2020, 2, 21): ra - 22,
+        datetime(current_year, 4, 21): ra - 2,
+        datetime(current_year, 5, 21): ra - 4,
+        datetime(current_year, 6, 21): ra - 6,
+        datetime(current_year, 7, 21): ra - 8,
+        datetime(current_year, 8, 21): ra - 10,
+        datetime(current_year, 9, 21): ra - 12,
+        datetime(current_year, 10, 21): ra - 14,
+        datetime(current_year, 11, 21): ra - 16,
+        datetime(current_year, 12, 21): ra - 18,
+        datetime(current_year, 1, 21): ra - 20,
+        datetime(current_year, 2, 21): ra - 22,
 
     }
 
     # Find the closest date to today in the dictionary time_of_year
-    today = datetime.today()
+
+    # print(today.year)
     base = timedelta(days=366)
     for k, v in time_of_year.items():
         delta = k - today
@@ -79,7 +83,7 @@ def star_rising_time(ra, sun_times: dict):
         if star_rise < 0:
             star_rise += 24
     elif - 1 <= closest_delay <= 1:
-        return 'This star is not observable now'
+        return 'This star is not observable now because it rises and sets along with the Sun'
     if star_rise == 24:
         star_rise -= 24
     star_set = star_rise + 12
@@ -90,8 +94,10 @@ def star_rising_time(ra, sun_times: dict):
     day = [d for d in range(sunrise, sunset)]
     star_span = [h for h in range(star_rise, star_set)]
 
+    current_ra = str(closest_delay) + right_ascension[2:]
+
     return {'star rise': star_rise, 'star set': star_set,
-            'day': day, 'star span': star_span, 'closest delay': closest_delay}
+            'day': day, 'star span': star_span, 'current ra': current_ra}
 
 
 # TODO improve calculate position
@@ -118,7 +124,7 @@ def calculate_position(rise_time: int, utc: int):
 # API call for coordinates from city name -- OpenCage Geocoding
 def geocoding_api(city):
     url_geocoding = 'https://api.opencagedata.com/geocode/v1/json?'
-    api_key = ''
+    api_key = secret_keys['GEO_APIKEY']
     payload = {'q': city, 'key': api_key, 'limit': 1}
     request = requests.get(url_geocoding, params=payload)
     file = request.json()
